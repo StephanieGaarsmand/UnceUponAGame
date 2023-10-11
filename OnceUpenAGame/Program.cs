6d64332dc7f494ba229fea216f53a8790610fc80
TheBadAngel <elisabethelida@hotmail.com>
2023-10-11 14:27:20 +0200

    Implementeret metode

--- OnceUpenAGame/Program.cs
+++ OnceUpenAGame/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            ReachANumber reachANumber = new ReachANumber();
+            /*reachANumber.CalculateCurrentValue(10);
+            reachANumber.CalculateCurrentValue(2);
+            reachANumber.CalculateCurrentValue(5);*/
+            foreach (Operator op in reachANumber.Operators)
+                Console.Write($"{op}, ");
+            
+            //reachANumber.Operators.ForEach(Console.WriteLine);
+            Console.ReadLine();
         }
     }
 }
--- OnceUpenAGame/ReachANumber.cs
+++ OnceUpenAGame/ReachANumber.cs
@@ -9,22 +9,56 @@ namespace OnceUpenAGame
     public class ReachANumber
     {
         public int TargetValue;
-        public int CurrentValue;
-        public Operator Operator;
-        public int Round;
+        public int CurrentValue = 0;
+        public List<Operator> Operators = new List<Operator>();
+        public int Round=0;
         public int RoundLimit = 7;
         private Random random = new Random();
 
-        public void CalculateCurrentValue(int selectedNumber) 
+        public ReachANumber()
         {
-            Operator = (Operator) random.Next(0,4); // Vi tager vores operator og tilfældiggøre den mellem 0-3
+            TargetValue = random.Next(51);
+            for (int i = 0; i < RoundLimit; i++)
+            {
+                Operators.Add((Operator)random.Next(0, 4)); // Vi tager vores operator og tilfældiggøre den mellem 0-3
+            }
+        }
+
+
+        public void CalculateCurrentValue(int selectedNumber)
+        {
+
+            switch (Operators[Round])
+            {
+                case Operator.add:
+                    CurrentValue += selectedNumber;
+                    break;
+
+                case Operator.subtract:
+                    CurrentValue -= selectedNumber;
+                    break;
+
+                case Operator.multiply:
+                    CurrentValue *= selectedNumber;
+                    break;
+
+                case Operator.divide:
+                    CurrentValue = (int)Math.Round((double)CurrentValue / selectedNumber);
+                    break;
+
+            }
+            Round++;
+            Console.WriteLine(CurrentValue);
+
+
+
             // Udregn nuævrende værdi udfra det valgte nummer og tilfældig operator
             // Tjek om nummeret er ramt samt om runder er brugt
         }
-        
+
         public void SelectNumber()
         {
-            random.Next(0,);
+            random.Next(0, 51);
             CalculateCurrentValue(0); // Vis muligheder til brugeren. Modtag input fra bruger.
                                       // Vi kører metoden med det valgte input
 
