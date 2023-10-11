370cfc1b873e33e8ad198958a9c79a256d856dd9
Naywia <48431873+Naywia@users.noreply.github.com>
2023-10-11 17:24:22 +0200

    Added more classes and created some scenes

--- OnceUpenAGame/Program.cs
+++ OnceUpenAGame/Program.cs
@@ -1,15 +1,77 @@
-﻿namespace OnceUpenAGame
+﻿using System.Runtime.CompilerServices;
+
+namespace OnceUpenAGame
 {
     public class Program
     {
         static void Main(string[] args)
         {
+            Player player = new Player();
+            Item goldCoin = new Item();
+             
+            Option openInventory = new(
+                "Open Inventory",
+                () => player.OpenInventory()
+                );
+
+            #region Great Hall
+            Scene greatHall = new("The Hall Of Greatness",
+                "Description here",
+                new List<Scene>(),
+                new List<Option>() { openInventory },
+                true);
+            #endregion
+
+            #region Church
+            Scene church3 = new("Church",
+                "It's a big coin!",
+                new List<Scene>(),
+                new List<Option>() { new Option("Pick it up", ()=> goldCoin.PickUpItem()) });
+            Scene church2 = new("Church",
+                "Continuing to look around you spot another doorway",
+                new List<Scene>(),
+                new List<Option>() { openInventory });
+            Scene church = new("Church",
+                "Standing in the middle of the pews looking towards the alter, you see one of your family photos... Weird..\nIgnoring it you see something shiny on one of the pews.",
+                new List<Scene>(),
+                new List<Option>() { 
+                    new("Continue looking for things", ()=> church2.DisplayScene()),
+                    new("Examine the shiny thing", ()=> church3.DisplayScene()),
+                    openInventory });
+            #endregion
+            #region Locker room
+            Scene lockerroom2 = new("Locker room",
+                "Continuing to look around you spot another doorway",
+                new List<Scene>(),
+                new List<Option>() {openInventory });
+            Scene lockerroom = new("Locker room",
+                "It looks like this locker room belongs to a bunch of miners. You see the names \"Bashful\" and \"Grumpy\" on some of the cubbies, odd names.. They remind you of something... You can't remember what...",
+                new List<Scene>(),
+                new List<Option>() { 
+                    new Option("Continue looking around", ()=> lockerroom2.DisplayScene()),
+                    openInventory });
+            #endregion
+
+            Scene cave = new("Cave",
+                "Inside the cave you see more of the spiders crawling under a big door in front of you.\nLooking around you see two others open doorways to each side of you.",
+                new List<Scene>() { church, lockerroom, greatHall },
+                new List<Option>() {openInventory});
+
+            Scene prolgue = new("Prologue",
+                "You're standing outside a dark ominous looking cave, with small (adorable?) spiders crawling near the entrance.",
+                new List<Scene>() {cave},
+                new List<Option>() { new ("Leave the cave alone", () => Environment.Exit(0)) });
+
+            
+
 
             ReachANumber reachANumber = new ReachANumber();
             /*reachANumber.CalculateCurrentValue(10);
             reachANumber.CalculateCurrentValue(2);
             reachANumber.CalculateCurrentValue(5);*/
-            
+
+            prolgue.DisplayScene();
+
 
             reachANumber.SelectNumber();
             //reachANumber.Operators.ForEach(Console.WriteLine);
---
+++ OnceUpenAGame/Scene.cs
@@ -0,0 +1,126 @@
+﻿using Figgle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnceUpenAGame
+{
+    public class Scene
+    {
+        Scene? previousScene;
+        List<Option> options = new();
+        public string Name;
+        public string Description;
+        bool locked;
+
+        public Scene(string name, string description, List<Scene> nearestScenes, List<Option> options, bool locked = false, Scene? previousScene = null)
+        {
+            Name = name;
+            Description = description;
+            this.locked = locked;
+            this.previousScene = previousScene;
+
+            foreach (Scene scene in nearestScenes)
+            {
+                this.options.Add(new($"Go to {scene.Name}", () => scene.GoToScene(this)));
+            }
+
+            options.ForEach(this.options.Add);
+            this.locked = locked;
+        }
+
+        public void GoToScene(Scene previousScene)
+        {
+            this.previousScene = previousScene;
+            DisplayScene();
+        }
+
+        public void DisplayScene()
+        {
+            if (!locked)
+            {
+                int index = 0;
+                // Write the menu out
+                WriteOptions(options[index]);
+
+                // Store key info in here
+                ConsoleKeyInfo keyinfo;
+                do
+                {
+                    keyinfo = Console.ReadKey();
+
+                    // Handle each key input (down arrow will write the menu again with a different selected item)
+                    if (keyinfo.Key == ConsoleKey.DownArrow)
+                    {
+                        if (index + 1 < options.Count)
+                        {
+                            index++;
+                            WriteOptions(options[index]);
+                        }
+                    }
+                    if (keyinfo.Key == ConsoleKey.UpArrow)
+                    {
+                        if (index - 1 >= 0)
+                        {
+                            index--;
+                            WriteOptions(options[index]);
+                        }
+                    }
+                    // Handle different action for the option
+                    if (keyinfo.Key == ConsoleKey.Enter)
+                    {
+                        options[index].Selected.Invoke();
+                        index = 0;
+                    }
+                }
+                while (keyinfo.Key != ConsoleKey.X);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("The door is locked, try to find a key.");
+                Thread.Sleep(3000);
+                previousScene.DisplayScene();
+            }
+        }
+        void WriteOptions(Option selectedOption)
+        {
+            Console.Clear();
+            Console.WriteLine(FiggleFonts.Epic.Render(Name));
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"\n{Description}\n\n");
+            Console.ForegroundColor = ConsoleColor.White;
+            foreach (Option option in options)
+            {
+                if (option == selectedOption)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.Write("<... ");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.Write("     ");
+                }
+
+                Console.Write(option.Name);
+
+                if (option == selectedOption)
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                    Console.WriteLine(" ...>");
+                    Console.ForegroundColor = ConsoleColor.White;
+                }
+                else
+                {
+                    Console.WriteLine("     ");
+                }
+            }
+        }
+
+    }
+}
